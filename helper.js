e4a23d4f63ae2a6772353743150e1f0c9f2ceed9
Vorbert Kruk <pryrokopter@gmail.com>
2020-02-02 17:18:15 +0100

    Dodano sprawdzanie url-a oraz zmieniono uprawnienia, tak by url-e wewnętrzne przeglądarki nie wyrzucały error-ów

--- helper.js
+++ helper.js
@@ -1,8 +1,10 @@
 console.log('Wyszukiwacz gotowy do akcji!');
-chrome.tabs.onUpdated.addListener(tabId => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  tab.url &&
+    !tab.url.includes('chrome://') &&
     setTimeout(() => {
-        chrome.tabs.executeScript(tabId, {
-            file: './functionality.js'
-        });
+      chrome.tabs.executeScript(tabId, {
+        file: './functionality.js',
+      });
     }, 1000);
 });
